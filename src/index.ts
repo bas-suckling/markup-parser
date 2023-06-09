d14a7d0fd4ce7e90d148cc56b545fb630ed2500f
bas-suckling <sebastian.suckling@compono.com>
2023-06-10 11:22:32 +1200

    Update program entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,16 @@
 import { checkTags } from './reducerCheckTags';
 
-console.log('Running against sample input:');
-console.log(
-  checkTags('The following text<C><B>is centred and in boldface</B></C>'),
-);
+const input = [
+  'The following text<C><B>is centred and in boldface</B></C>',
+  '<B><C> This should be centred and in boldface, but the tags are wrongly nested </B></C>',
+  String.raw`<B>This <\g>is <B>boldface</B> in <<*> </> a</B> <\6> <<d>sentence`,
+  '<B>This should be in boldface, but there is an extra closing tag</B></C>',
+  '<B>This should be in boldface, but there is an extra closing tag</B></C>',
+  '<B><C>This should be centred and in boldface, but there is a missing closing tag</C>',
+];
 
-console.log(
-  checkTags(
-    String.raw`<B>This <\g>is <B>boldface</B> in <<*> </> a</B> <\6> <<d>sentence`,
-  ),
-);
-
-console.log(
-  checkTags(
-    '<B><C> This should be centred and in boldface, but the tags are wrongly nested </B></C>',
-  ),
-);
-
-console.log(
-  checkTags(
-    '<B>This should be in boldface, but there is an extra closing tag</B></C>',
-  ),
-);
-
-console.log(
-  checkTags(
-    '<B><C>This should be centred and in boldface, but there is a missing closing tag</C>',
-  ),
-);
+console.log('Sample Inputs \n');
+input.forEach((paragraph) => {
+  console.log(`Input: ${paragraph}`);
+  console.log(`Output: ${checkTags(paragraph)} \n`);
+});
